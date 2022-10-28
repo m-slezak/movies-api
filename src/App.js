e35e4ffc3d86ea778c9556802603739179a3d48c
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-10-28 08:39:33 +0200

    inital styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import './App.css';
+
 
 function App() {
 
@@ -30,7 +30,7 @@ function App() {
       }
     };
 
-    fetch(`https://imdb8.p.rapidapi.com/auto-complete?q=${endPoint}`, options)
+    fetch(`https://imdb8.p.rapidapi.com/auto-complete?q=+${endPoint}`, options)
       .then(response => response.json())
       .then(data => setContainer(data.d) )
       .catch(err => console.error(err));
@@ -41,16 +41,32 @@ function App() {
   return (
     <div className="App">
 
-      <form onSubmit={onSubmitHandler}>
-        <input type="text" value={endPoint} onChange={handleEndPoint} />
-        <button type="submit">Search</button>
+      <form onSubmit={onSubmitHandler}
+      className="flex mt-8 items-center justify-center">
+
+        <input
+         type="text"
+         value={endPoint}
+         onChange={handleEndPoint}
+         className="min-w-[600px] px-4 h-[40px] text-2xl ring-offset-2  ring-4 outline-none rounded-xl "
+           />
+
+        <button
+        type="submit"
+        className='px-4 py-2 ml-6 bg-orange-100 text-black border-spacing-2 hover:bg-orange-300 rounded-md '
+        >Search</button>
       </form>
 
       {container.map((item) => {
         return (
-        <div >
-          <img src={item.i.imageUrl} alt="" srcset="" />
-          <p> {item.l} </p>
+        <div>
+          <div>
+          <img src={item.i.imageUrl}
+          className="w-[160px] h-[160px]"
+          alt="" srcset=""
+           />
+          <p className='text-white'> {item.l} </p>
+          </div>
         </div>
       )})}
 
